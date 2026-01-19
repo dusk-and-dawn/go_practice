bc1e8e83bcd6eec9ef4caa2a1024d9722d5dd0dd
David Koeller <david.koeller@volteuropa.org>
2026-01-20 00:04:42 +0100

    this language is cool

--- hello_go/hello.go
+++ hello_go/hello.go
@@ -6,6 +6,24 @@ import (
 	"rsc.io/quote"
 )
 
+// func main() {
+// 	fmt.Println(quote.Go())
+// }
+
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+// 	})
+
+// 	http.ListenAndServe(":8080", nil)
+// }
+
 func main() {
-	fmt.Println(quote.Go())
+	for i := 0; i < 3; i++ {
+		fmt.Println(quote.Go())
+	}
+
+	// func test() {
+
+	// }
 }
