742e52e85495f52e2f48029be80c1a5ceebe8022
David Koeller <david.koeller@volteuropa.org>
2026-01-21 00:45:26 +0100

    now with AI call to optimize user input and a simple html template to render the result relatively pretty for Alain

--- hello_go/go.mod
+++ hello_go/go.mod
@@ -2,7 +2,10 @@ module example/hello_go
 
 go 1.25.6
 
-require github.com/openai/openai-go/v3 v3.16.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/openai/openai-go/v3 v3.16.0
+)
 
 require (
 	github.com/tidwall/gjson v1.18.0 // indirect
--- hello_go/hello.go
+++ hello_go/hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/openai/openai-go/v3"
 	"github.com/openai/openai-go/v3/option"
 	"github.com/openai/openai-go/v3/responses"
@@ -16,6 +18,17 @@ import (
 // }
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	key := os.Getenv("OPENAI_API_KEY")
+	if key == "" {
+		fmt.Println("OPENAI_API_KEY not set")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			http.Error(w, "illegal method - send ICE", http.StatusMethodNotAllowed)
@@ -30,22 +43,37 @@ func main() {
 			return
 		}
 
+		context2 := "Imagine you are part of a HR software in which a user is currently entering text describing a team they want to add. It is your task to return a text which includes all facts from their prompt, but optimize it as regards business language, details that they omitted. Their prompt: "
 		text := r.FormValue("optimizable_text")
-		fmt.Fprintf(w, "got: %q\n", text)
+		// fmt.Fprintf(w, "got: %q\n", text)
 		fmt.Println(text)
 
 		client := openai.NewClient(
-			option.WithAPIKey(os.Getenv("OPENAI_API_KEY")),
+			option.WithAPIKey(key),
 		)
 
-		resp, err := client.Responses.New(context.TODO(), openai.ResponseNewParams{
-			Model: "gpt-5-nano",
-			Input: responses.ResponseNewParamsInputUnion{OfString: openai.String(text)},
-		})
+		resp, err := client.Responses.New(
+			context.TODO(),
+			responses.ResponseNewParams{
+				Model: "gpt-5-nano",
+				Input: responses.ResponseNewParamsInputUnion{OfString: openai.String(context2 + "" + text)},
+			})
 		if err != nil {
 			panic(err.Error())
 		}
 		fmt.Println(resp.OutputText())
+		// fmt.Fprintf(w, "got: %q\n", resp.OutputText())
+
+		var tmpl = template.Must(
+			template.ParseFiles("static/result.html"),
+		)
+
+		tmpl.Execute(w, struct {
+			Output string
+		}{
+			Output: resp.OutputText(),
+		})
+
 	})
 
 	http.ListenAndServe(":8080", nil)
