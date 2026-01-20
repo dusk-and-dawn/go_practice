f37f204054d49fd3d18ec8b6a85a987d6b55b512
David Koeller <david.koeller@volteuropa.org>
2026-01-20 21:05:47 +0100

    now ready to take in text and print it to the page and terminal - yey for small wins

--- hello_go/hello.go
+++ hello_go/hello.go
@@ -3,27 +3,42 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"net/http"
 )
 
 // func main() {
 // 	fmt.Println(quote.Go())
 // }
 
-// func main() {
-// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-// 	})
-
-// 	http.ListenAndServe(":8080", nil)
-// }
-
 func main() {
-	for i := 0; i < 3; i++ {
-		fmt.Println(quote.Go())
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "illegal method - send ICE", http.StatusMethodNotAllowed)
+			return
+		}
+		http.ServeFile(w, r, "static/main.html")
+	})
 
-	// func test() {
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
 
-	// }
+		text := r.FormValue("optimizable_text")
+		fmt.Fprintf(w, "got: %q\n", text)
+		fmt.Println(text)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
+
+// func main() {
+// 	for i := 0; i < 3; i++ {
+// 		fmt.Println(quote.Go())
+// 	}
+
+// 	// func test() {
+
+// 	// }
+// }
