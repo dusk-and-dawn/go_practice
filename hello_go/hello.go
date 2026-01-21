1e99cc67d56bd0289f71886689c621e5d99692ee
David Koeller <david.koeller@volteuropa.org>
2026-01-21 22:23:03 +0100

    bit of prompt engineering

--- hello_go/hello.go
+++ hello_go/hello.go
@@ -43,7 +43,7 @@ func main() {
 			return
 		}
 
-		context2 := "Imagine you are part of a HR software in which a user is currently entering text describing a team they want to add. It is your task to return a text which includes all facts from their prompt, but optimize it as regards business language, details that they omitted. Their prompt: "
+		context2 := "Imagine you are part of a HR software in which a user is currently entering text describing a team they want to add. It is your task to return a text which includes all facts from their prompt, but optimize it as regards business language and details that they omitted. Please always create a basic role description for each mentioned role, and elaborate on general categories, if given. Do not exceed 250 words and do not treat it as a dialogue, treat your answer like it would be the final team description to go into the software. Their prompt: "
 		text := r.FormValue("optimizable_text")
 		// fmt.Fprintf(w, "got: %q\n", text)
 		fmt.Println(text)
